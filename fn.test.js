d723378db42c20761b400b79338656a19e1995dd
hermes7144 <kichancho@gmail.com>
2022-08-27 18:28:07 +0900

    jest 비동기 테스트

--- fn.test.js
+++ fn.test.js
@@ -1,55 +1,14 @@
 const fn = require('./fn');
 
-// toBeNull
-// toBeUndefined
-// toBeDefined
-
-test('null은 null입니다', () => {
-  expect(null).toBeNull();
+test('3초 후 나이 30.', async () => {
+  await expect(fn.getAge()).resolves.toBe(30);
 });
 
-// toBeTruthy
-// toBeFalsy
-
-test('비어있지 않은 문자열은 true 입니다.', () => {
-  expect(fn.add('hello', 'world')).toBeTruthy();
+test('3초 후 나이 30.', async () => {
+  const age = await fn.getAge();
+  return expect(age).toBe(31);
 });
 
-// toBeGreaterThan 크다
-// toBeGreaterThanOrEqual 크거나 같다
-// toBeLesserThan 작다
-// toBeLesserThanOrEqual 작거나 같다
-
-test('ID는 10자 이하여야 합니다.', () => {
-  const id = 'THE_BLACK';
-  expect(id.length).toBeLessThanOrEqual(10);
-});
-
-test('비밀번호 4자리', () => {
-  const pw = '1234';
-  expect(pw.length).toBe(4);
-});
-
-test('비밀번호 4자리', () => {
-  const pw = '1234';
-  expect(pw.length).toEqual(4);
-});
-
-test('0.1 더하기 0.2는 0.3입니다.', () => {
-  expect(fn.add(0.1, 0.2)).toBeCloseTo(0.3);
-});
-
-test('Hello World에 a라는 글자가 있나?', () => {
-  expect('Hello World').toMatch(/h/i);
-});
-
-// toContain
-test('유저 리스트에 Mike가 있나?', () => {
-  const user = 'Mike';
-  expect(['Mike', 'Jake']).toContain(user);
-});
-
-// error check
-test('이거 에러 나나요?', () => {
-  expect(() => fn.throwErr()).toThrow('xx');
+test('0 + 1은 1이야.', () => {
+  expect(fn.add(0, 1)).toBe(1);
 });
